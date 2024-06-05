28ef744016e57e544209c3a8630a713fcd2273ab
David Hernandez <david.hernandez@fineos.com>
2024-06-05 12:18:42 +0200

    First commit

---
+++ consumer/src/test/java/com/github/davidherdu/microservices/integration/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.davidherdu.microservices.integration.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
